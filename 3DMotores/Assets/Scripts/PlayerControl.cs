cd05a2cb55c2d684ab3e9a0574cb13aa6ce478b2
iviniciuss7 <oliveira.igor@escolar.ifrn.edu.br>
2023-09-22 14:25:51 -0300

    MovimentaçãoPlayer

--- 3DMotores/Assets/Scripts/PlayerControl.cs
+++ 3DMotores/Assets/Scripts/PlayerControl.cs
@@ -1,36 +1,51 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.UIElements;
 
 public class PlayerControl : MonoBehaviour
 {
     public float speed;
     private CharacterController charCtrl;
     private Animator anim;
-
+    private Transform cam;
+    private Vector3 moveDirection;
+    public float gravity;
     public float smoothRotTime;
     private float turnSmoothVelocity;
+    
     void Start()
     {
         charCtrl = GetComponent<CharacterController>();
         anim = GetComponent<Animator>();
+        cam = Camera.main.transform;
     }
 
     // Update is called once per frame
     void Update()
     {
-        float horizontal = Input.GetAxisRaw("Horizontal");
-        float vertical = Input.GetAxisRaw("Vertical");
+        Move();
+    }
 
+    private void Move()
+    {
+        if (charCtrl.isGrounded)
+        { 
+            float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
         Vector3 direction = new Vector3(horizontal, 0f, vertical);
 
         if (direction.magnitude > 0)
         {
-            float angle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
+            float angle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
             float smoothAngle = Mathf.SmoothDampAngle(transform.eulerAngles.y, angle, ref turnSmoothVelocity, smoothRotTime);
             transform.rotation = Quaternion.Euler(0f, smoothAngle, 0f);
-            charCtrl.Move(direction * speed * Time.deltaTime);
+            moveDirection = Quaternion.Euler(0f, angle, 0f) * Vector3.forward;
             anim.SetInteger("transition", 1);
         }
+        }
+        moveDirection.y -= gravity * Time.deltaTime;
+        charCtrl.Move(moveDirection * speed * Time.deltaTime);
     }
 }
