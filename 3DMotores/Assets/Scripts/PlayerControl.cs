0a78d63b321d84515c420daa50d01243e1f69f27
iviniciuss7 <oliveira.igor@escolar.ifrn.edu.br>
2023-09-29 16:14:38 -0300

    Animações + Attack

--- 3DMotores/Assets/Scripts/PlayerControl.cs
+++ 3DMotores/Assets/Scripts/PlayerControl.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -14,7 +15,10 @@ public class PlayerControl : MonoBehaviour
     public float gravity;
     public float smoothRotTime;
     private float turnSmoothVelocity;
-    
+    public float colliderRadius;
+    public List<Transform> enemyList = new List<Transform>();
+    public bool isWalking;
+
     void Start()
     {
         charCtrl = GetComponent<CharacterController>();
@@ -26,9 +30,9 @@ public class PlayerControl : MonoBehaviour
     void Update()
     {
         Move();
+        GetMouseInput();
     }
-
-    private void Move()
+    void Move()
     {
         if (charCtrl.isGrounded)
         { 
@@ -38,14 +42,91 @@ public class PlayerControl : MonoBehaviour
 
         if (direction.magnitude > 0)
         {
-            float angle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
-            float smoothAngle = Mathf.SmoothDampAngle(transform.eulerAngles.y, angle, ref turnSmoothVelocity, smoothRotTime);
-            transform.rotation = Quaternion.Euler(0f, smoothAngle, 0f);
-            moveDirection = Quaternion.Euler(0f, angle, 0f) * Vector3.forward;
-            anim.SetInteger("transition", 1);
+            if (!anim.GetBool("attacking"))
+            {
+                float angle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+                float smoothAngle = Mathf.SmoothDampAngle(transform.eulerAngles.y, angle, ref turnSmoothVelocity, smoothRotTime);
+                transform.rotation = Quaternion.Euler(0f, smoothAngle, 0f);
+                moveDirection = Quaternion.Euler(0f, angle, 0f) * Vector3.forward  * speed;
+                anim.SetInteger("transition", 1);
+                isWalking = true;
+            }
+
+            else
+            {
+                anim.SetBool("walking", false);
+                moveDirection = Vector3.zero;
+            }
+             
         }
+        
+        else if (isWalking)
+        {
+            anim.SetBool("walking", false);
+            moveDirection = Vector3.zero;
+            anim.SetInteger("transition", 0);
+            isWalking = false;
+        }
+        
         }
         moveDirection.y -= gravity * Time.deltaTime;
-        charCtrl.Move(moveDirection * speed * Time.deltaTime);
+        charCtrl.Move(moveDirection * Time.deltaTime);
+    }
+
+    void GetMouseInput()
+    {
+        if (charCtrl.isGrounded)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                if (anim.GetBool("walking"))
+                {
+                    anim.SetBool("walking", false);
+                    anim.SetInteger("transition", 0);
+                }
+
+                if (!anim.GetBool("walking"))
+                {
+                    StartCoroutine("Attack");
+                }
+            }
+        }
+    }
+
+    IEnumerator Attack()
+    {
+        anim.SetBool("attacking", true);
+        anim.SetInteger("transition", 2);
+        yield return new WaitForSeconds(0.5f);
+            
+        GetEnemiesList();
+        
+        foreach (Transform enems in enemyList)
+        {
+            Debug.Log(enems.name);
+        }
+
+        yield return new WaitForSeconds(1.25f);
+        
+        anim.SetInteger("transition", 0);
+        anim.SetBool("attacking", false);
+    }
+
+    void GetEnemiesList()
+    {
+        enemyList.Clear();
+        foreach (Collider coll in Physics.OverlapSphere((transform.position + transform.forward * colliderRadius), colliderRadius))
+        {
+            if (coll.gameObject.CompareTag("Enemy"))
+            {
+                enemyList.Add(coll.transform);
+            }
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position + transform.forward, colliderRadius);
     }
 }
